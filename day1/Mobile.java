91ddca013c09199a1a195bd842b8d307cc9e0a99
Megavarshi <142220902+Megavarshi@users.noreply.github.com>
2023-08-14 17:12:40 +0530

    Add files via upload
    Day 1

---
+++ day1/Mobile.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int noofsimcards=2;
+boolean isCharged=true;
+String model="vivo";
+char logo='v';
+float weight= 200.80f;
+
+System.out.println("noofsimcards:" +noofsimcards);
+System.out.println("isCharged:" +true);
+System.out.println("model:" +model);
+System.out.println("logo:" +logo);
+System.out.println("weight:" +weight);
+
+
+
+
+
+
+
+
+
+
+	   }
+
+}
